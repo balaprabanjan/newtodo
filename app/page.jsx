404b783d9ab5a3bd6ee211546e99cf571e818d50
gautamkumar30 <gautamkumarganesan@gmail.com>
2024-12-08 15:15:21 +0530

    fixed tw config

--- app/page.jsx
+++ app/page.jsx
@@ -1,51 +1,55 @@
-'use client'
+"use client";
+
 import { useState } from "react";
 import TodoInput from "@/component/TodoInput";
 import TodoList from "@/component/TodoList";
+
 export default function Home() {
-    const [tasks, setTasks] = useState([]);
-    const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
 
-    const addTask = () => {
-        if (taskInput.trim()) {
-            const newTask = {
-                id: Date.now(),
-                text: taskInput,
-                dateTime: new Date().toLocaleString(),
-                completed: false,
-            };
-            setTasks([...tasks, newTask]);
-            setTaskInput(""); // Clear input field
-        }
-    };
+  const addTask = () => {
+    if (taskInput.trim()) {
+      const newTask = {
+        id: Date.now(),
+        text: taskInput,
+        dateTime: new Date().toLocaleString(),
+        completed: false,
+      };
+      setTasks([...tasks, newTask]);
+      setTaskInput("");
+    }
+  };
 
-    const toggleTask = (id) => {
-        setTasks(
-            tasks.map((task) =>
-                task.id === id ? { ...task, completed: !task.completed } : task
-            )
-        );
-    };
-
-    const deleteTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id));
-    };
-
-    return (
-        <div className="bg-gradient-to-tl from-zinc-500 via-white to-zinc-500 min-h-screen flex items-center justify-center">
-            <div className="container mx-auto my-80 w-4/12 rounded-2xl px-7 bg-black flex flex-col">
-                <h1 className="text-2xl font-bold text-center mb-4 text-orange-500">To-Do List</h1>
-                <TodoInput
-                    taskInput={taskInput}
-                    setTaskInput={setTaskInput}
-                    addTask={addTask}
-                />
-                <TodoList
-                    tasks={tasks}
-                    toggleTask={toggleTask}
-                    deleteTask={deleteTask}
-                />
-            </div>
-        </div>
+  const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
     );
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="bg-gradient-to-tl from-zinc-500 via-white to-zinc-500 min-h-screen flex items-center justify-center">
+      <div className="container mx-auto my-80 w-4/12 rounded-2xl px-7 bg-black flex flex-col">
+        <h1 className="text-2xl font-bold text-center mb-4 text-orange-500">
+          To-Do List
+        </h1>
+        <TodoInput
+          taskInput={taskInput}
+          setTaskInput={setTaskInput}
+          addTask={addTask}
+        />
+        <TodoList
+          tasks={tasks}
+          toggleTask={toggleTask}
+          deleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
 }
--- component/TodoInput.jsx
+++ component/TodoInput.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 export default function TodoInput({ taskInput, setTaskInput, addTask }) {
-    return (
-        <div className="flex justify-between gap-2 mb-4">
-            <input
-                type="text"
-                id="taskInput"
-                placeholder="Enter a new task..."
-                value={taskInput}
-                onChange={(e) => setTaskInput(e.target.value)}
-                className="flex-1 px-4 py-2 border border-gray-300 rounded-lg"
-            />
-            <button
-                onClick={addTask}
-                className="text-white px-4 py-2 rounded-lg hover:bg-orange-600"
-            >
-                Add Task
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex justify-between gap-2 mb-4">
+      <input
+        type="text"
+        id="taskInput"
+        placeholder="Enter a new task..."
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        className="block flex-1 px-4 py-2 border border-gray-300 rounded-lg"
+      />
+      <button
+        onClick={addTask}
+        className="text-white px-4 py-2 rounded-lg hover:bg-orange-600 "
+      >
+        Add Task
+      </button>
+    </div>
+  );
 }
